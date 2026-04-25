5f3140dfd5650ff8c669ad05b253f540a99319bc
Traincape <traincapetech0641@gmail.com>
2026-04-25 13:16:43 +0530

    Update logo mismatch issue

--- fix_wrap.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-let html = fs.readFileSync('d:/lem_chai/menu.html', 'utf8');
-
-// Replace w-16 text-right with whitespace-nowrap min-w-[75px] text-right
-html = html.replace(/w-16 text-right/g, 'min-w-[80px] text-right whitespace-nowrap');
-
-fs.writeFileSync('d:/lem_chai/menu.html', html, 'utf8');
-console.log('Fixed wrap issues');
